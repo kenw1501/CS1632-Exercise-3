ee4867c644c9dea850ed96fe8b6fbd56a4b3c50f
ken woodruff <kjhw9979@gmail.com>
2017-10-04 13:07:14 -0400

    Adds 1 test for ListCats

--- RentACat_Test.java
+++ RentACat_Test.java
@@ -1,6 +1,10 @@
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 import org.junit.*;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
 
 public class RentACat_Test {
 
@@ -12,6 +16,23 @@ public class RentACat_Test {
 
 
     // Add Tests
+    @Test
+    public void testListCats() {
+        Cat testCat = new Cat(0, "Meow");
+
+        ArrayList<Cat> ListOfCats = new ArrayList<Cat>();
+        ListOfCats.add(testCat);
+
+        Cat mockCat = Mockito.mock(Cat.class);
+        when(mockCat.getName()).thenReturn("Meow");
+        when(mockCat.getId()).thenReturn(0);
+
+
+        RentACat r = new RentACat();
+        String result = r.listCats(ListOfCats);
+
+        assert(result.equals("ID 0. Meow"));
+    }
 
 
 }
